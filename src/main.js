0048cd5ae5e9726189dc6f655951d86a3c68d350
Ernestas Kvedaras <kvedarasernestas@gmail.com>
2020-08-22 11:36:32 +0200

    init: tailwind

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store'
 import router from './router'
+import './assets/tailwind.css'
 
 createApp(App).use(router).use(store).mount('#app')
