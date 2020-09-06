341055896100465d51e65e1108b33c40424cabba
Ernestas Kvedaras <kvedarasernestas@gmail.com>
2020-09-06 16:14:22 +0200

    Add key search to hash values

--- src/components/Content/HashContent.vue
+++ src/components/Content/HashContent.vue
@@ -1,23 +1,62 @@
 <template>
   <div class="p-4">
+    <div class="relative flex items-center mb-2">
+      <!--suppress HtmlFormInputWithoutLabel -->
+      <input type="text" placeholder="Search keys..." v-model="search" class="p-2 rounded shadow w-full"/>
+      <Spinner :class="[isLoading ? 'opacity-100' : 'opacity-0']"/>
+    </div>
     <JsonRenderer :data="value"/>
+    <button @click="loadMore" v-if="nextCursor" class="underline rounded transition duration-200 ease-in-out hover:bg-white hover:shadow hover:no-underline m-2 p-1">Load more...</button>
   </div>
 </template>
 
 <script>
 import { redis } from '@/services/redis'
 import JsonRenderer from '@/components/Renderer/JsonRenderer'
+import Spinner from '@/components/Elements/Spinner'
+import _ from 'lodash'
 
 export default {
   name: 'HashContent',
-  components: { JsonRenderer },
+  components: { Spinner, JsonRenderer },
   props: ['name'],
   data: () => ({
-    value: ''
+    value: '',
+    search: '',
+    isLoading: true,
+    nextCursor: 0,
   }),
   async mounted () {
-    this.value = await redis.async('hgetall', this.name)
-  }
+    await this.loadKeys()
+  },
+  watch: {
+    search () {
+      this.loadKeys({ pattern: `*${this.search}*` })
+    },
+  },
+  methods: {
+    loadKeys ({ pattern = '*', cursor = 0, limit = redis.pageSize, lastLoad = 0 } = {}) {
+      this.isLoading = true
+      return redis.async('hscan', this.name, cursor, 'MATCH', pattern, 'COUNT', limit).then(result => {
+        result.lastLoad = Object.keys(result[1]).length
+        this.nextCursor = parseInt(result[0])
+
+        let value = _.fromPairs(_.chunk(result[1], 2))
+        this.value = cursor ? { ...this.value, ...value } : value
+
+        return result
+      }).then(result => {
+        if (result.nextCursor && lastLoad + result.lastLoad < limit) {
+          return this.loadKeys({ pattern, cursor: result.nextCursor, limit, lastLoad: lastLoad + Object.keys(result[1]).length })
+        }
+
+        return result
+      }).finally(() => this.isLoading = false)
+    },
+    loadMore () {
+      this.loadKeys({ pattern: `*${this.search}*`, cursor: this.nextCursor })
+    },
+  },
 }
 </script>
 
