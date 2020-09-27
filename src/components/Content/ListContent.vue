f26147fc7a4400b636f37b655d295d5a8943b3df
Ernestas Kvedaras <kvedarasernestas@gmail.com>
2020-09-27 21:57:51 +0200

    Ability to edit list values

--- src/components/Content/ListContent.vue
+++ src/components/Content/ListContent.vue
@@ -11,11 +11,20 @@
       </div>
     </div>
     <div class="overflow-y-auto h-full pb-10 rounded overflow-x-hidden">
-      <div v-for="(item, i) in filtered" :key="i" class="relative">
-        <button type="button" @click="deleteItem(item, i)" class="absolute top-0 right-0 mt-2 mr-2">
-          <DeleteIcon class="w-5 cursor-pointer text-gray-500 hover:text-redis"/>
-        </button>
-        <ValueRenderer :value="item" class="mb-4"/>
+      <div v-for="(item, i) in filtered" :key="i">
+        <div class="relative" v-if="!isEditing[i]">
+          <button type="button" @click="editItem(item, i)" class="absolute top-0 right-0 mt-2 mr-8">
+            <EditIcon class="w-5 cursor-pointer text-gray-500 hover:text-redis"/>
+          </button>
+          <button type="button" @click="deleteItem(item, i)" class="absolute top-0 right-0 mt-2 mr-2">
+            <DeleteIcon class="w-5 cursor-pointer text-gray-500 hover:text-redis"/>
+          </button>
+          <ValueRenderer :value="item" class="mb-4"/>
+        </div>
+        <div v-if="isEditing[i]">
+          <textarea class="p-2 w-full shadow h-64" :ref="`editor_${i}`" v-model="editValue" @keydown.esc="close(i)" @keydown.ctrl.enter="save(i)"/>
+          <span class="text-xs text-gray-500">CTRL + Enter to save, Esc to cancel</span>
+        </div>
       </div>
       <button @click="loadMore" v-if="start" class="underline rounded transition duration-200 ease-in-out hover:bg-white hover:shadow hover:no-underline m-2 p-1">Load more...</button>
     </div>
@@ -30,17 +39,20 @@ import AddIcon from '@/components/Icons/AddIcon'
 import AddKeyModal from '@/components/Modals/AddKeyModal'
 import DeleteIcon from '@/components/Icons/DeleteIcon'
 import { EventBus } from '@/services/eventBus'
+import EditIcon from '@/components/Icons/EditIcon'
 
 export default {
   name: 'ListContent',
-  components: { DeleteIcon, AddIcon, Spinner, ValueRenderer },
+  components: { EditIcon, DeleteIcon, AddIcon, Spinner, ValueRenderer },
   props: ['name'],
   data: () => ({
     value: [],
     size: 0,
     start: 0,
     isLoading: true,
+    isEditing: [],
     search: '',
+    editValue: '',
   }),
   computed: {
     filtered () {
@@ -87,6 +99,20 @@ export default {
     },
     showKeyAddModal () {
       this.$modal.show(AddKeyModal, { fill: { name: this.name, type: 'list' } })
+    },
+    editItem (value, index) {
+      this.editValue = value
+      this.$set(this.isEditing, index, true)
+      this.$nextTick(() => this.$refs[`editor_${index}`][0].focus())
+    },
+    save (index) {
+      redis.async('lset', this.name, index, this.editValue)
+          .then(() => this.value[index] = this.editValue)
+          .then(() => this.$toasted.success('Saved'))
+          .finally(() => this.close(index))
+    },
+    close (index) {
+      this.$set(this.isEditing, index, false)
     },
     deleteItem (value, index) {
       this.$modal.show('dialog', {
