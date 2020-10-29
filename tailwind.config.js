b31aef7b88a0e60836d7051f49e58774b8356e64
Ernestas Kvedaras <kvedarasernestas@gmail.com>
2020-10-29 19:51:49 +0100

    Style toasted notifications

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,18 @@ module.exports = {
       },
       colors: {
         'white-10p': 'rgba(255, 255, 255, 0.1)',
-        'redis': '#da2c21',
+        'redis': {
+          default: '#D92D20',
+          '100': '#F8D1CE',
+          '200': '#F1A7A2',
+          '300': '#EA7D75',
+          '400': '#E45349',
+          '500': '#D92D20',
+          '600': '#AD241A',
+          '700': '#811B13',
+          '800': '#54110D',
+          '900': '#280806',
+        },
         'redis-gray': {
           50: '#F6F6F6',
           100: '#ECECEC',
@@ -20,12 +31,36 @@ module.exports = {
           800: '#1E1E1E',
           900: '#141414',
         },
-      }
+        'jungle-green': {
+          default: '#28C347',
+          '100': '#C4F3CD',
+          '200': '#99EAAA',
+          '300': '#6FE286',
+          '400': '#45D962',
+          '500': '#28C347',
+          '600': '#1F9837',
+          '700': '#176E28',
+          '800': '#0E4419',
+          '900': '#051909',
+        },
+        'picton-blue': {
+          default: '#349BEF',
+          '100': '#F1F8FE',
+          '200': '#C2E1FA',
+          '300': '#92C9F6',
+          '400': '#63B2F2',
+          '500': '#349BEF',
+          '600': '#1282DE',
+          '700': '#0E66AF',
+          '800': '#0A4B7F',
+          '900': '#072F50',
+        },
+      },
     },
   },
   variants: {},
   plugins: [],
   future: {
-    removeDeprecatedGapUtilities: true
-  }
+    removeDeprecatedGapUtilities: true,
+  },
 }
