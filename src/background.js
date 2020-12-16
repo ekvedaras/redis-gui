cf8f8e2a559e389eeaa5a2d68f4e6ea9db24cb03
Ernestas Kvedaras <kvedarasernestas@gmail.com>
2020-12-16 19:01:43 +0100

    fix: remove window frame (needed for windows)

--- src/background.js
+++ src/background.js
@@ -21,6 +21,7 @@ function createWindow () {
   win = new BrowserWindow({
     width: 1200,
     height: 800,
+    frame: false,
     titleBarStyle: 'hidden',
     webPreferences: {
       // Use pluginOptions.nodeIntegration, leave this alone
