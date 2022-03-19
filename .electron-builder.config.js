bbcb0fa1700c455d0fccd4b76cdf6a55ab7ac7fd
Ernestas Kvedaras <kvedarasernestas@gmail.com>
2022-03-19 14:05:21 +0100

    Remove computed app version

--- .electron-builder.config.js
+++ .electron-builder.config.js
@@ -1,8 +1,3 @@
-if (process.env.VITE_APP_VERSION === undefined) {
-  const now = new Date;
-  process.env.VITE_APP_VERSION = `${now.getUTCFullYear() - 2000}.${now.getUTCMonth() + 1}.${now.getUTCDate()}-${now.getUTCHours() * 60 + now.getUTCMinutes()}`;
-}
-
 /**
  * @type {import('electron-builder').Configuration}
  * @see https://www.electron.build/configuration/configuration
@@ -15,9 +10,6 @@ const config = {
   files: [
     'packages/**/dist/**',
   ],
-  extraMetadata: {
-    version: process.env.VITE_APP_VERSION,
-  },
 };
 
 module.exports = config;
